c648604f21c065df055f4d2dc849b2b14de0d95c
kobrak1 <burakkarhan657@gmail.com>
2023-12-22 01:49:15 +0300

    planetInfo.jsx is added, user can search a planet by its name for a short description of it

--- src/components/Apps/PlanetInfo/PlanetInfo.jsx
+++ src/components/Apps/PlanetInfo/PlanetInfo.jsx
@@ -1,35 +1,64 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const PlanetInfo = () => {
+  const [planetName, setPlanetName] = useState("");
   const [planetData, setPlanetData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/', {
-          headers: {
-            'X-RapidAPI-Key': 'a47c302fc8mshd0e1ddd2de5234bp183471jsn35a31c74da0c',
-            'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com',
-          },
-        });
+        const response = await axios.get(
+          "https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/",
+          {
+            headers: {
+              "X-RapidAPI-Key":
+                "a47c302fc8mshd0e1ddd2de5234bp183471jsn35a31c74da0c",
+              "X-RapidAPI-Host": "planets-info-by-newbapi.p.rapidapi.com",
+            },
+          }
+        );
         setPlanetData(response.data);
       } catch (error) {
-        console.error('Error fetching planet data:', error);
+        console.error("Error:", error);
       }
     };
-
     fetchData();
-  }, []); // Empty dependency array means this effect runs once after the initial render
+  }, []);
+
+  console.log(planetData);
+  console.log(typeof planetData);
+
+  // handle input
+  const handleInput = (e) => {
+    const input = e.target.value.toLowerCase();
+    setPlanetName(input.charAt(0).toUpperCase() + input.slice(1));
+  };
 
   return (
-    <>
-      {planetData ? (
-        <div>{planetData[0].description}</div>
-      ) : (
-        <div>Loading...</div>
+    <div>
+      <input
+        type="text"
+        value={planetName}
+        onChange={handleInput}
+      />
+      {planetData && (
+        <div className="show-data">
+          {planetData.map((data, index) => (
+            <div key={index}>
+              {data.name === planetName && (
+                <>
+                  <p>{data.description}</p>
+                  <div>
+                    <img src={data.imgSrc.img} alt={`image of ${planetName}`} style={{width:"10%"}} />
+                  </div>
+                </>
+              )}
+            </div>
+          ))}
+        </div>
       )}
-    </>
+    </div>
   );
 };
 
