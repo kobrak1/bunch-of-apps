a42d666c2b899ac3412b7e9cb0dd6d18512042cb
kobrak1 <burakkarhan657@gmail.com>
2023-12-29 17:54:59 +0300

    update: File name altered from mainProvider.jsx to MainProvider.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,16 @@
+import { NavLink } from "react-router-dom";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
   return (
     <aside>
       <div className="component-container">
-        <a href="./">Image Carousel</a>
-        <a href="./planet-info">Planet Info</a>
-        <a href="./faq">FAQ Accordion</a>
-        <a href="./quote-generator">Quote Generator</a>
-        <a href="./shopping-list">Shopping List</a>
-        <a href="./github-user-search">Github User Search</a>
-        <a href="./video-player">Video Player</a>
-        <a href="./bmi-calculator">BMI Calculator</a>
+        <NavLink to={"/planet-info"}> <p>Planet Info</p> </NavLink>
+        <NavLink to={"/quote-generator"}> <p>Quote Generator</p> </NavLink>
+        <NavLink to={"/shopping-list"}> <p>Shopping List</p> </NavLink>
+        <NavLink to={"/github-user-search"}> <p>GitHub User Search</p> </NavLink>
+        <NavLink to={"/video-player"}> <p>Video Player</p> </NavLink>
+        <NavLink to={"/bmi-calculator"}> <p>BMI Calculator</p> </NavLink>
       </div>
     </aside>
   );
