ff279fb39fb94538227e6d399efc018e08dc103d
kobrak1 <burakkarhan657@gmail.com>
2023-12-29 17:42:45 +0300

    update: File name altered from mainProvider.jsx to MainProvider.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,14 +4,14 @@ const Sidebar = () => {
   return (
     <aside>
       <div className="component-container">
-        <a href="/">Image Carousel</a>
-        <a href="/planet-info">Planet Info</a>
-        <a href="/faq">FAQ Accordion</a>
-        <a href="/quote-generator">Quote Generator</a>
-        <a href="/shopping-list">Shopping List</a>
-        <a href="/github-user-search">Github User Search</a>
-        <a href="/video-player">Video Player</a>
-        <a href="/bmi-calculator">BMI Calculator</a>
+        <a href="./">Image Carousel</a>
+        <a href="./planet-info">Planet Info</a>
+        <a href="./faq">FAQ Accordion</a>
+        <a href="./quote-generator">Quote Generator</a>
+        <a href="./shopping-list">Shopping List</a>
+        <a href="./github-user-search">Github User Search</a>
+        <a href="./video-player">Video Player</a>
+        <a href="./bmi-calculator">BMI Calculator</a>
       </div>
     </aside>
   );
