084db9be1e826c68493bbee2ea57c78eb050f5af
kobrak1 <burakkarhan657@gmail.com>
2023-12-31 14:38:12 +0300

    update: Sideba.jsx is removed from unnecessary p tags

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,15 +5,14 @@ const Sidebar = () => {
   return (
     <aside>
       <div className="component-container">
-        <NavLink to={"/"}> <p>Image Carousel</p> </NavLink>
-        <NavLink to={"/planet-info"}> <p>Planet Info</p> </NavLink>
-        <NavLink to={"/quote-generator"}> <p>Quote Generator</p> </NavLink>
-        <NavLink to={"/shopping-list"}> <p>Shopping List</p> </NavLink>
-        <NavLink to={"/github-user-search"}> <p>GitHub User Search</p> </NavLink>
-        <NavLink to={"/video-player"}> <p>Video Player</p> </NavLink>
-        <NavLink to={"/bmi-calculator"}> <p>BMI Calculator</p> </NavLink>
-      </div>
-    </aside>
+        <NavLink to={"/"}> Image Carousel </NavLink>
+        <NavLink to={"/planet-info"}> Planet Info </NavLink>
+        <NavLink to={"/quote-generator"}> Quote Generator </NavLink>
+        <NavLink to={"/shopping-list"}> Shopping List </NavLink>
+        <NavLink to={"/github-user-search"}> GitHub User Search </NavLink>
+        <NavLink to={"/video-player"}> Video Player </NavLink>
+        <NavLink to={"/bmi-calculator"}> BMI Calculator </NavLink>
+      </div>    </aside>
   );
 };
 
