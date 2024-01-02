ccb72ae854ad9eb5721dcb4640143c298deb175e
kobrak1 <burakkarhan657@gmail.com>
2024-01-03 00:31:06 +0300

    update: products array is updated

--- src/components/Apps/ProductTable/ProductTable.jsx
+++ src/components/Apps/ProductTable/ProductTable.jsx
@@ -12,9 +12,15 @@ const ProductTable = () => {
     { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
     { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
     { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Fruits", price: "$6", stocked: false, name: "Banana" },
+    { category: "Fruits", price: "$3", stocked: false, name: "Strawberry" },
     { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
     { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
     { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+    { category: "Dairy", price: "$1", stocked: true, name: "Milk" },
+    { category: "Dairy", price: "$4", stocked: false, name: "Cheese" },
+    { category: "Dairy", price: "$5", stocked: true, name: "Yoghut" },
+    { category: "Dairy", price: "$2", stocked: true, name: "Butter" },
   ];
 
   // a function that extracts the category names from products arrat as a set
